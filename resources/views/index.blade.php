c30db9fe02e0ccb7b46c479978599201f06d20ff
DzKriMo <amamra.mehdiakram@gmail.com>
2024-03-12 13:57:49 +0100

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,78 @@
+<html>
+
+<head>
+    <title>Nafes نفس</title>
+</head>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=ABeeZee:ital@0;1&display=swap" rel="stylesheet">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/index.css">
+
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #f0f0f0; border-radius: 15px; margin: 5px;">
+        <!-- Logo and text -->
+        <a class="navbar-brand" href="#">Nafes</a>
+
+        <!-- Toggler button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Navbar items -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">&nbsp;&nbsp;About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#advice">&nbsp;&nbsp;Advice</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#faq">&nbsp;&nbsp;FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#reviews">&nbsp;&nbsp;Reviews</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">&nbsp;&nbsp;Contact</a>
+                </li>
+                <li class="nav-item">
+                &nbsp;
+                    <a href="{{ route('login') }}" class="nav-button">
+                    
+                    <button class="nav-button ">Login</button>
+                    </a>
+                
+                </li>
+                <li class="nav-item">
+                &nbsp;
+                </li>
+                <li class="nav-item">
+                &nbsp;   
+                <a href="{{ route('register') }}" class="nav-button">
+                     <button class="nav-button ">Apply as a Therapist</button>
+                    </a>
+                   
+                </li>
+                <li class="nav-item">
+                &nbsp;
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+       
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
