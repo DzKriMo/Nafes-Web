71c06d11f883f722c9a62b2893bb61a519668fc5
DzKriMo <amamra.mehdiakram@gmail.com>
2024-04-05 16:35:33 +0100

    fixed the audio shit

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,23 @@
 let CurrentUserId;
 let otherUser; 
+
+let currentDate = new Date();
+
+                
+let day = currentDate.getDate();
+let month = currentDate.getMonth() + 1; 
+let year = currentDate.getFullYear();
+let hours = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+
+
+day = day < 10 ? '0' + day : day;
+month = month < 10 ? '0' + month : month;
+hours = hours < 10 ? '0' + hours : hours;
+minutes = minutes < 10 ? '0' + minutes : minutes;
+
+
+let formattedDateTime = `${day}/${month}/${year} ${hours}:${minutes}`;
 const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
 
@@ -61,7 +79,7 @@ function startChat(userId, selectedUser) {
         });
     });
 
-    listenForMessages();
+    //listenForMessages();
 }
 
 function sendMessage(userId, message, file = null, audioBlob = null) {
@@ -72,6 +90,7 @@ function sendMessage(userId, message, file = null, audioBlob = null) {
         senderId: CurrentUserId,
         receiverId: userId,
         message: message,
+        timestamp: formattedDateTime,
     };
 
     if (file) {
@@ -111,66 +130,67 @@ function sendMessage(userId, message, file = null, audioBlob = null) {
     }
 }
 
+const displayedMessages = new Set();
+
 function listenForMessages() {
     const chatRef = database.ref('Chats').child(CurrentUserId).child(otherUser);
 
     chatRef.on('child_added', (snapshot) => {
         const message = snapshot.val();
-        displayMessage(message);
+        if (!displayedMessages.has(snapshot.key)) {
+            displayedMessages.add(snapshot.key); 
+            displayMessage(message);
+        }
     });
 }
 
 function displayMessage(message) {
     const chatMessages = document.getElementById('chatMessages');
-    const existingMessages = chatMessages.querySelectorAll('.message');
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
 
-    // Checking if the message already exists in the chat
-    const isDuplicate = Array.from(existingMessages).some((existingMessage) => {
-        return existingMessage.textContent.trim() === message.message.trim();
-    });
-
-    if (!isDuplicate) {
-        const messageElement = document.createElement('div');
-        messageElement.classList.add('message');
-
-        if (message.senderId === CurrentUserId) {
-            messageElement.classList.add('sent-message');
-        } else {
-            messageElement.classList.add('received-message');
-        }
-
-        if (message.messageLink) {
-            const linkElement = document.createElement('a');
-            linkElement.href = message.messageLink;
-            linkElement.textContent = message.message;
-
-            linkElement.style.fontWeight = 'bold';
-            linkElement.style.textDecoration = 'underline';
-            linkElement.style.color = 'blue';
-
-            linkElement.addEventListener('mouseenter', () => {
-                linkElement.style.color = 'darkblue';
-            });
-            linkElement.addEventListener('mouseleave', () => {
-                linkElement.style.color = 'blue';
-            });
-
-            messageElement.appendChild(linkElement);
-        } else if (message.audioUrl) {
-            const audioElement = document.createElement('audio');
-            audioElement.src = message.audioUrl;
-            audioElement.controls = true;
-
-            messageElement.textContent = message.message;
-            messageElement.appendChild(audioElement);
-        } else {
-            messageElement.textContent = message.message;
-        }
-
-        chatMessages.appendChild(messageElement);
-        chatMessages.scrollTop = chatMessages.scrollHeight;
+    if (message.senderId === CurrentUserId) {
+        messageElement.classList.add('sent-message');
+    } else {
+        messageElement.classList.add('received-message');
     }
+
+    if (message.messageLink) {
+        const linkElement = document.createElement('a');
+        linkElement.href = message.messageLink;
+        linkElement.textContent = message.message;
+        applyLinkStyles(linkElement);
+
+        messageElement.appendChild(linkElement);
+    } else if (message.audioUrl) {
+        const audioElement = document.createElement('audio');
+        audioElement.src = message.audioUrl;
+        audioElement.controls = true;
+
+        messageElement.textContent = message.message;
+        messageElement.appendChild(audioElement);
+    } else {
+        messageElement.textContent = message.message;
+    }
+
+    chatMessages.appendChild(messageElement);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 }
+
+
+function applyLinkStyles(linkElement) {
+    linkElement.style.fontWeight = 'bold';
+    linkElement.style.textDecoration = 'underline';
+    linkElement.style.color = 'blue';
+
+    linkElement.addEventListener('mouseenter', () => {
+        linkElement.style.color = 'darkblue';
+    });
+    linkElement.addEventListener('mouseleave', () => {
+        linkElement.style.color = 'blue';
+    });
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchUsers();
@@ -178,7 +198,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const fileUploadInput = document.getElementById('file-upload');
     const audioRecordBtn = document.getElementById('recordButton');
     const audioPlayer = document.getElementById('audio-player');
+    const uploadTrigger = document.getElementById('uploadTrigger');
 
+
+    uploadTrigger.addEventListener('click', function() {
+        fileUploadInput.click(); 
+      });
     fileUploadInput.addEventListener('change', (event) => {
         const file = event.target.files[0];
         if (file) {
@@ -186,7 +211,53 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    audioRecordBtn.addEventListener('click', async () => {
+
+
+
+
+    const recordButton = document.getElementById('recordButton');
+    let mediaRecorder;
+    let audioChunks = [];
+    
+    recordButton.addEventListener('mousedown', async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            mediaRecorder = new MediaRecorder(stream);
+    
+            mediaRecorder.addEventListener('dataavailable', (event) => {
+                if (event.data.size > 0) {
+                    audioChunks.push(event.data);
+                }
+            });
+    
+            mediaRecorder.addEventListener('stop', async () => {
+                const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+    
+                
+                await sendMessage(otherUser, formattedDateTime, null, audioBlob);
+    
+               
+                audioPlayer.src = URL.createObjectURL(audioBlob);
+            });
+    
+            mediaRecorder.start();
+        } catch (error) {
+            console.error('Error recording audio:', error);
+        }
+    });
+    
+    recordButton.addEventListener('mouseup', () => {
+        if (mediaRecorder && mediaRecorder.state === 'recording') {
+            mediaRecorder.stop();
+            audioChunks = []; // Clear audioChunks for the next recording
+        }
+    });
+    
+
+
+
+
+    /*audioRecordBtn.addEventListener('click', async () => {
         try {
             const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
             const mediaRecorder = new MediaRecorder(stream);
@@ -204,27 +275,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-                let currentDate = new Date();
-
-                
-                let day = currentDate.getDate();
-                let month = currentDate.getMonth() + 1; 
-                let year = currentDate.getFullYear();
-                let hours = currentDate.getHours();
-                let minutes = currentDate.getMinutes();
-                
-                
-                day = day < 10 ? '0' + day : day;
-                month = month < 10 ? '0' + month : month;
-                hours = hours < 10 ? '0' + hours : hours;
-                minutes = minutes < 10 ? '0' + minutes : minutes;
-                
-                
-                let formattedDateTime = `${day}/${month}/${year} ${hours}:${minutes}`;
-
-
-
-
                 sendMessage(otherUser, formattedDateTime, null, audioBlob);
 
                 audioPlayer.src = URL.createObjectURL(audioBlob);
@@ -238,7 +288,7 @@ document.addEventListener('DOMContentLoaded', () => {
         } catch (error) {
             console.error('Error recording audio:', error);
         }
-    });
+    });*/
 
     document.getElementById('sendMessageBtn').addEventListener('click', () => {
         const messageInput = document.getElementById('messageInput');
